9b46189d8683d698ea16dc98c95e311671b0e21a
Riccardo Frascio <riccardo.frascio@studio.unibo.it>
2022-10-25 17:54:43 +0200

    Implemented methods in SocialNetworkUserImpl.java

---
+++ src/main/java/it/unibo/collections/social/impl/SocialNetworkUserImpl.java
@@ -0,0 +1,116 @@
+/**
+ * 
+ */
+package it.unibo.collections.social.impl;
+
+import it.unibo.collections.social.api.SocialNetworkUser;
+import it.unibo.collections.social.api.User;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * 
+ * This will be an implementation of
+ * {@link SocialNetworkUser}:
+ * 1) complete the definition of the methods by following the suggestions
+ * included in the comments below.
+ * 
+ * @param <U>
+ *            Specific {@link User} type
+ */
+public final class SocialNetworkUserImpl<U extends User> extends UserImpl implements SocialNetworkUser<U> {
+
+    /*
+     *
+     * [FIELDS]
+     *
+     * Define any necessary field
+     *
+     * In order to save the people followed by a user organized in groups, adopt
+     * a generic-type Map:  think of what type of keys and values would best suit the requirements
+     */
+    private Map<U, String> followedUser = new HashMap<>(); //map key is user , map value is the circle
+    
+    /*
+     * [CONSTRUCTORS]
+     *
+     * 1) Complete the definition of the constructor below, for building a user
+     * participating in a social network, with 4 parameters, initializing:
+     *
+     * - firstName
+     * - lastName
+     * - username
+     * - age and every other necessary field
+     */
+    /**
+     * Builds a user participating in a social network.
+     *
+     * @param name
+     *            the user firstname
+     * @param surname
+     *            the user lastname
+     * @param userAge
+     *            user's age
+     * @param user
+     *            alias of the user, i.e. the way a user is identified on an
+     *            application
+     */
+    public SocialNetworkUserImpl(final String name, final String surname, final String user, final int userAge) {
+        super(null, null, null, 0);
+    }
+    
+    /*
+     * 2) Define a further constructor where the age defaults to -1
+     */
+    public SocialNetworkUserImpl(String name, String surname, String user) {
+        super(name, surname, user, -1);
+    }
+    /*
+     * [METHODS]
+     *
+     * Implements the methods below
+     */
+    @Override
+    public boolean addFollowedUser(final String circle, final U user) {
+        
+        for(var u : followedUser.keySet()) {
+            if(user.equals(u)) {
+                return false;        
+            }
+        }
+        followedUser.put(user, circle);
+        return true;
+    }
+
+    /**
+     *
+     * [NOTE] If no group with groupName exists yet, this implementation must
+     * return an empty Collection.
+     */
+    @Override
+    public Collection<U> getFollowedUsersInGroup(final String groupName) {
+        Collection<U> followedUserGroup = new ArrayList<>();
+
+        for(var u : this.followedUser.keySet()) {
+            if(followedUser.get(u) == groupName) {
+                followedUserGroup.add(u);   
+            }
+        }
+        return followedUserGroup;
+    }
+
+    @Override
+    public List<U> getFollowedUsers() {
+        List<U> followed = new ArrayList<>();
+        for(var u : this.followedUser.keySet()) {
+            followed.add(u);
+        }
+        return followed;
+    }
+
+    
+}
